e1bde133ed348dd7ac0a51d7b3eb3ee862f8f765
jnangwafor <43428966+jnangwafor@users.noreply.github.com>
2018-10-03 16:40:00 -0500

    Add files via upload

---
+++ testGit.php
@@ -0,0 +1,66 @@
+<?php
+	
+	echo "<h1>Welcome to WDV341--Intro to PHP</h1>";
+	echo "<h2>Jackson Angwafor</h2>"; 
+
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>WDV341 Intro to PHP</title>
+	<style>
+		
+		#container
+		{
+			width: 960px;
+			background-color:#B2E2F3;
+			margin: auto;
+			border-radius: 25px;
+			padding: 10px;
+		}
+	#h3style
+		{
+			text-align: center;
+		}
+		body
+		{
+			font-family: Cambria, "Hoefler Text", "Liberation Serif", Times, "Times New Roman", "serif";
+			font-size: 16px;
+		}
+		
+		
+	</style>
+</head>
+
+<body>
+	<div id="container">
+		<h3 id="h3style">Git Related Terms</h3>
+		<p>
+			<ol>
+				<li><h4>Version control software</h4></h3></li>
+				<p>Version control is a system that records files or a set of files over time , so that specific versions can be recall later.</p>
+				<li><h4>Add</h4></li>
+				<p>Git add command add files or a project to a git repository. First a new repository should be created.</p>
+				<li><h4>Commit</h4></li>
+				<p>Commit saves and records individual change or set of changes to a file and issues a commit ID</p>
+				<li><h4>Push</h4></li>
+				<p>Pushing send committed change to a remote git repository which can be access by others after pull request.</p>
+				<li><h4>Pull</h4></li>
+				<p>Pull request let others know changes that have been pushed to GitHub. Those changes can now be access by everyone if its public.</p>
+				<li><h4>Clone</h4></li>
+				<p>Cloning a Git repository is creating a  a local copy of code provided by a developer. This is done by using the Git clone command.</p>
+				
+		
+		</ol>
+		
+		</p>
+	
+	
+	
+	
+	</div>
+	
+</body>
+</html>
